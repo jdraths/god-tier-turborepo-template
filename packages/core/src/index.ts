4b13b5610f17cd511acbaae0b56496947a4eb99b
Roane <5256269+jdraths@users.noreply.github.com>
2025-06-25 00:56:21 -0500

    add database

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,7 @@
+import { prisma, User } from "@repo/db";
+
 export const runPromise = async () => {
+  const user: User | null = await prisma.user.findFirst();
   const awaited = await Promise.resolve(1);
   return awaited;
 };
